734dcf47b876bb10b258bce5103f57fce88d4c4c
Mahmood Choopani <choopani.m81@gmail.com>
2022-09-16 16:23:02 +0430

    change authentication systems

--- core/settings.py
+++ core/settings.py
@@ -158,7 +158,7 @@ API_GENERATOR = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.BasicAuthentication'
     ],
 }
