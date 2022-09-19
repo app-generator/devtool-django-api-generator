ec851bef9d5429ab222c5c080dfa80a139fa4b3a
Mahmood Choopani <choopani.m81@gmail.com>
2022-09-19 20:21:12 +0430

    fix csrf token header

--- apps/home/views.py
+++ apps/home/views.py
@@ -6,8 +6,10 @@ Copyright (c) 2019 - present AppSeed.us
 from django import template
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 from django.template import loader
 from django.urls import reverse
+
 
 @login_required(login_url="/login/")
 def index(request):
